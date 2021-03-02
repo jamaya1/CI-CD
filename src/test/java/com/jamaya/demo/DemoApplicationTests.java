53324059f45d4ed399d7a301e04ef960671d2e9c
Jonathan Amaya <jonartur.amaya@gmail.com>
2021-03-01 21:03:22 -0600

    initial import

---
+++ src/test/java/com/jamaya/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamaya.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
