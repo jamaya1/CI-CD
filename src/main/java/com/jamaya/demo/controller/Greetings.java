6887602e3cba79c80205994238366832c8eb9abc
Jonathan Amaya <jonartur.amaya@gmail.com>
2021-03-01 22:00:53 -0600

    agregando servicio de saludo

---
+++ src/main/java/com/jamaya/demo/controller/Greetings.java
@@ -0,0 +1,13 @@
+package com.jamaya.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greetings {
+
+	@GetMapping(path="/saludo")
+	public String saludo() {
+		return "Hola mundo";
+	}
+}
---
+++ src/test/java/com/jamaya/demo/controller/GreetingsTest.java
@@ -0,0 +1,15 @@
+package com.jamaya.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GreetingsTest {
+
+	@Test
+	void saludoTest() {
+		assertEquals("Hola mundo", new Greetings().saludo());
+	}
+}
